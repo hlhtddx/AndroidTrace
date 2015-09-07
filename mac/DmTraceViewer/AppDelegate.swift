79e1f84ebe9251b96652323dcb80355e433609b6
Frank Zhang <frankzhang123@gmail.com>
2015-09-07 10:46:03 +0800

    New project for MacOSX application

---
+++ mac/DmTraceViewer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DmTraceViewer
+//
+//  Created by Frank Zhang on 15/9/7.
+//  Copyright (c) 2015年 Frank Zhang. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
